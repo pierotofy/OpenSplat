e72278f2a4e2311a341117942cba2d768b05ea17
Piero Toffanin <pt@masseranolabs.com>
2024-02-06 13:08:27 -0500

    CPU execution

--- model.hpp
+++ model.hpp
@@ -13,7 +13,7 @@ namespace ns{
 torch::Tensor randomQuatTensor(long long n);
 
 struct Model : torch::nn::Module {
-  Model(const Points &points) {
+  Model(const Points &points, const torch::Device &device) {
     long long numPoints = points.xyz.size(0); 
     const int shDegree = 3;
     torch::manual_seed(42);
@@ -23,20 +23,16 @@ struct Model : torch::nn::Module {
     quats = register_parameter("quats", randomQuatTensor(numPoints), true);
 
     int dimSh = numShBases(shDegree);
-    torch::Tensor shs = torch::zeros({numPoints, dimSh, 3}, torch::kFloat32);
-    // TODO: send to CUDA?
+    torch::Tensor shs = torch::zeros({numPoints, dimSh, 3}, torch::TensorOptions().dtype(torch::kFloat32).device(device));
 
     shs.index({Slice(), 0, Slice(None, 3)}) = rgb2sh(points.rgb.toType(torch::kFloat64) / 255.0).toType(torch::kFloat32);
     shs.index({Slice(), Slice(1, None), Slice(3, None)}) = 0.0f;
 
     featuresDc = register_parameter("featuresDc", shs.index({Slice(), 0, Slice()}), true);
     featuresRest = register_parameter("featuresRest", shs.index({Slice(), Slice(1, None), Slice()}), true);
-
-    // TODO: these should be in CUDA?
-
     opacities = register_parameter("opacities", torch::logit(0.1f * torch::ones({numPoints, 1})), true);
     
-    backgroundColor = torch::Tensor({0.0f, 0.0f, 0.0f}); // Black
+    backgroundColor = torch::tensor({0.0f, 0.0f, 0.0f}); // Black
 
   }
 
--- opensplat.cpp
+++ opensplat.cpp
@@ -9,10 +9,19 @@ using namespace torch::indexing;
 int main(int argc, char *argv[]){
     std::string projectRoot = "banana";
     const float downScaleFactor = 2.0f;
+    torch::Device device = torch::kCPU;
+
+    if (torch::cuda::is_available()) {
+        std::cout << "Using CUDA" << std::endl;
+        device = torch::kCUDA;
+    }else{
+        std::cout << "Using CPU" << std::endl;
+    }
 
     ns::InputData inputData = ns::inputDataFromNerfStudio(projectRoot);
     ns::rescaleOutputResolution(inputData.cameras, 1.0f / downScaleFactor);
     
-    ns::Model m(inputData.points);
+    ns::Model m(inputData.points, device);
+    m.to(device);
 
 }
