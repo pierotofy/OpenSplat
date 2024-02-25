e8bd73733e68cc246741328c6e751b49c8c49e9c
Pengfei Xuan <pxuan@agerpoint.net>
2024-02-24 23:10:53 -0500

    Patch NvToolsExt

--- .github/workflows/cuda/Windows.ps1
+++ .github/workflows/cuda/Windows.ps1
@@ -1,6 +1,3 @@
-
-# Took from https://github.com/isl-org/Open3D
-
 # CUDA major and minor version
 $CUDA_VER_FULL = $args[0]
 $CUDA_VER_ARR = $CUDA_VER_FULL.Split(".")
@@ -28,6 +25,31 @@ $CUDA_VER_SHORT = "cu$($CUDA_VER_ARR[0])$($CUDA_VER_ARR[1])"
 #Remove-Item -Force -Recurse -LiteralPath "C:\msys64\"
 #Remove-Item -Force -Recurse -LiteralPath  "C:\mingw64\"
 
+# Install NvToolsExt
+$NVTOOLSEXT_PATH  = "C:\Program Files\NVIDIA Corporation\NvToolsExt"
+if (Test-Path -Path "$NVTOOLSEXT_PATH " -PathType Container) {
+Write-Output "Existing nvtools installation already found, continuing..."
+return
+}
+function New-TemporaryDirectory() {
+  New-TemporaryFile | ForEach-Object { Remove-Item $_; New-Item -ItemType Directory -Path $_ }
+}
+$NV_TOOLS_URL = "https://ossci-windows.s3.amazonaws.com/NvToolsExt.7z" # pytorch test-infra
+$tmpToolsDl = New-TemporaryFile
+Write-Output "Downloading NvTools, $NV_TOOLS_URL"
+Invoke-WebRequest -Uri "$NV_TOOLS_URL" -OutFile "$tmpToolsDl"
+$tmpExtractedNvTools = New-TemporaryDirectory
+7z x "$tmpToolsDl" -o"$tmpExtractedNvTools"
+Write-Output "Copying NvTools, '$tmpExtractedNvTools' -> '$NVTOOLSEXT_PATH'"
+New-Item -Path "$NVTOOLSEXT_PATH "-ItemType "directory" -Force
+Copy-Item -Recurse -Path "$tmpExtractedNvTools\*" -Destination "$NVTOOLSEXT_PATH"
+Remove-Item "$tmpExtractedNvTools" -Recurse -Force
+Remove-Item "$tmpToolsDl" -Recurse -Force
+
+$NVTOOLSEXT_PATH = "C:\Program Files\NVIDIA Corporation\NvToolsExt"
+echo "NVTOOLSEXT_PATH=$NVTOOLSEXT_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+
+
 # Network installer url
 if ( $CUDA_VER_ARR[0] -ge 11 ) {
 $CUDA_URL = "https://developer.download.nvidia.com/compute/cuda/$CUDA_VER_FULL/network_installers/cuda_$($CUDA_VER_FULL)_windows_network.exe"
@@ -53,9 +75,6 @@ if ( !$? ) {
 }
 
 Remove-Item .\cuda.exe -Force
-Get-PSDrive
-
-Get-ChildItem -Path "C:/Program Files" -Recurse -File -Filter "nvToolsExt.dll" -ErrorAction SilentlyContinue
 
 # Add CUDA environment variables.
 $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$CUDA_VER"
@@ -64,5 +83,4 @@ echo "CUDA_PATH_V$CUDA_VER_ID=$CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -
 echo "CUDA_VER_SHORT=$CUDA_VER_SHORT" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 echo "$CUDA_PATH\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 
-$NVTOOLSEXT_PATH = "C:\Program Files\NVIDIA Corporation\NvToolsExt"
-echo "NVTOOLSEXT_PATH=$NVTOOLSEXT_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+Get-PSDrive
