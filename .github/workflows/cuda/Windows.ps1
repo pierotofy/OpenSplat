8a9b8b5d098112a9ac2664c13f37216f1c7bc4b0
Pengfei Xuan <pxuan@agerpoint.net>
2024-02-25 22:46:28 -0500

    Perform full test

--- .github/workflows/cuda/Windows.ps1
+++ .github/workflows/cuda/Windows.ps1
@@ -5,26 +5,6 @@ $CUDA_VER = "$($CUDA_VER_ARR[0]).$($CUDA_VER_ARR[1])"
 $CUDA_VER_ID = "$($CUDA_VER_ARR[0])_$($CUDA_VER_ARR[1])"
 $CUDA_VER_SHORT = "cu$($CUDA_VER_ARR[0])$($CUDA_VER_ARR[1])"
 
-# Free up disk space
-# Docker Cache
-# docker rmi $(docker images -q -a)
-# Android SDK
-# if ($Env:ANDROID_HOME) {
-# Remove-Item -Recurse -Force $Env:ANDROID_HOME -ErrorAction Ignore
-# }
-# if ($Env:ANDROID_NDK_HOME) {
-# Remove-Item -Recurse -Force $Env:ANDROID_NDK_HOME -ErrorAction Ignore
-# }
-# # JVM
-# if ($Env:JAVA_HOME_11_X64) {
-# Remove-Item -Recurse -Force $Env:JAVA_HOME_11_X64 -ErrorAction Ignore
-# }
-# if ($Env:JAVA_HOME_8_X64) {
-# Remove-Item -Recurse -Force $Env:JAVA_HOME_8_X64 -ErrorAction Ignore
-# }
-#Remove-Item -Force -Recurse -LiteralPath "C:\msys64\"
-#Remove-Item -Force -Recurse -LiteralPath  "C:\mingw64\"
-
 # Network installer url
 if ( $CUDA_VER_ARR[0] -ge 11 ) {
 $CUDA_URL = "https://developer.download.nvidia.com/compute/cuda/$CUDA_VER_FULL/network_installers/cuda_$($CUDA_VER_FULL)_windows_network.exe"
@@ -44,21 +24,7 @@ echo "Downloading CUDA installer from $CUDA_URL"
 Invoke-WebRequest $CUDA_URL -OutFile cuda.exe
 echo "Installing CUDA..."
 Start-Process -Wait -FilePath .\cuda.exe -ArgumentList "$CUDA_INSTALL_ARGS"
-
-if ( !$? ) {
-  exit 1
-}
-
 Remove-Item .\cuda.exe -Force
-
-# Add CUDA environment variables.
-# $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$CUDA_VER"
-# echo "CUDA_PATH=$CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-# echo "CUDA_PATH_V$CUDA_VER_ID=$CUDA_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-# echo "CUDA_VER_SHORT=$CUDA_VER_SHORT" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-# echo "$CUDA_PATH\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
-# $TORCH_CUDA_ARCH_LIST = "7.0;7.5"
-# echo "TORCH_CUDA_ARCH_LIST=$TORCH_CUDA_ARCH_LIST" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 
 # Install NvToolsExt
 $NVTOOLSEXT_PATH  = "C:\Program Files\NVIDIA Corporation\NvToolsExt"
@@ -80,8 +46,3 @@ New-Item -Path "$NVTOOLSEXT_PATH "-ItemType "directory" -Force
 Copy-Item -Recurse -Path "$tmpExtractedNvTools\*" -Destination "$NVTOOLSEXT_PATH"
 Remove-Item "$tmpExtractedNvTools" -Recurse -Force
 Remove-Item "$tmpToolsDl" -Recurse -Force
-
-# $NVTOOLSEXT_PATH = "C:\Program Files\NVIDIA Corporation\NvToolsExt"
-# echo "NVTOOLSEXT_PATH=$NVTOOLSEXT_PATH" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-
-Get-PSDrive
