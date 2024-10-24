b00bc1a4a7b4d38fe0fb3cba769764197b9a42a0
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2024-10-24 17:01:08 +0900

    add docker-comopse.yml

---
+++ run_splat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script runs the SPLAT program with the given input file and output file
+# The input file is the first argument and the output file is the second argument
+
+# Check if the number of arguments is correct
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input .MOV file, or images/ directory"
+    exit 1
+fi
+
+# Check if the input is exists
+if [ -f "$1" && ]; then
+    # currently only support .MOV video files.
+    if [[ "$1" != *.MOV ]]; then
+        echo "Input file is not a .MOV file, so not supported"
+        exit 1
+    fi
+    # create 8 bit images using ffmpeg
+    IMAGES_DIR=$(dirname "$1")/images
+    mkdir -p $IMAGES_DIR
+    ffmpeg -ss 00:00:00 -to 00:00:10 -i "$1" -r 10 -vf "format=rgb24" ${IMAGES_DIR}/image%08d.png
+elif [ -d "$1" ]; then
+    IMAGES_DIR="$1"
+else
+    echo "Input file or directory does not exist"
+    exit 1
+fi
+
+# run colmap
+colmap automatic_reconstructor --image_path $IMAGES_DIR --workspace_path result
+
+# Run the SPLAT program with the input file and output file
+./opensplat $IMAGES_DIR -n 2000
