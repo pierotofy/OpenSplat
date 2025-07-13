6e2fc6a1a5276f735f12684acad8cc6ebe7fc057
Graham Reeves <graham@grahamreeves.com>
2025-07-13 15:49:54 +0100

    Added OpenSplat (swift) app target

---
+++ OpenSplatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenSplatApp
+//
+//  Created by Graham Reeves on 13/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenSplatApp/OpenSplatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSplatAppApp.swift
+//  OpenSplatApp
+//
+//  Created by Graham Reeves on 13/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct OpenSplatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
