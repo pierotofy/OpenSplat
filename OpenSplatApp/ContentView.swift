8b388c92b59d3ae267c0e66b94fd692980d6dfd9
Graham Reeves <graham@grahamreeves.com>
2025-07-13 17:59:14 +0100

    Added swift app to use framework

--- OpenSplatApp/ContentView.swift
+++ OpenSplatApp/ContentView.swift
@@ -7,18 +7,110 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+
+
+
+struct TrainerView : View 
+{
+	@StateObject var trainer : OpenSplatTrainer
+	@State var lastRenderImage : Image = Image(systemName: "clock.circle.fill")
+	@State var cameraIndex = 0
+	var renderImageSize = CGSize(width: 800, height: 600)
+	@State var someError : Error?
+	
+	var body: some View 
+	{
+		TrainingView()
+			.overlay
+		{
+			VStack(alignment: .leading)
+			{
+				TrainingStateView()
+				Spacer()
+			}
+			.frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .topLeading)
+		}
+		Button(action:OnClickedUpdateImage)
+		{
+			Text("Capture new image from camera \(cameraIndex)")
+		}
+	}
+	
+	@ViewBuilder func TrainingView() -> some View
+	{
+		lastRenderImage
+			//.resizable()
+			.scaledToFit()
+			.frame(maxWidth:.infinity,maxHeight: .infinity)
+			.background
+			{
+				Rectangle()
+					.fill(.blue)
+			}
+			.foregroundStyle(.white)
+	}
+	
+	@ViewBuilder func TrainingStateView() -> some View
+	{
+		if let error = someError
+		{
+			Text("Error: \(error.localizedDescription)")
+				.padding(5)
+				.background(.red)
+				.foregroundStyle(.white)
+		}
+		if let error = trainer.trainingError
+		{
+			Text("Error: \(error.localizedDescription)")
+				.padding(5)
+				.background(.red)
+				.foregroundStyle(.white)
+		}
+		else if trainer.isTraining
+		{
+			Text("Training...")
+				.padding(5)
+				.background(.green)
+				.foregroundStyle(.white)
+		}
+		else if trainer.isTraining
+		{
+			Text("Training Finished.")
+				.padding(5)
+				.background(.black)
+				.foregroundStyle(.white)
+		}
+	}
+	
+	func OnClickedUpdateImage()
+	{
+		UpdateImage(cameraIndex: cameraIndex)
+	}
+	
+	func UpdateImage(cameraIndex:Int)
+	{
+		do
+		{
+			let renderWidth = Int(renderImageSize.width)
+			let renderHeight = Int(renderImageSize.height)
+			let image = try trainer.RenderCamera(cameraIndex: cameraIndex, width:renderWidth, height: renderHeight)
+			let imageNs = NSImage(cgImage:image, size: .zero)
+			lastRenderImage = Image(nsImage: imageNs)
+		}
+		catch
+		{
+			someError = error
+		}	
+	}
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View 
+{
+	@State var trainer = OpenSplatTrainer(projectPath: "/Users/graham/Downloads/banana")
+
+	var body: some View 
+	{
+		TrainerView(trainer: trainer)
+	}
 }
+
---
+++ OpenSplatApp/OpenSplat.swift
@@ -0,0 +1,193 @@
+import OpenSplat
+import CoreGraphics
+import Accelerate
+
+public struct OpenSplatError : LocalizedError
+{
+	var message : String
+	
+	public init(_ message:String)
+	{
+		self.message = message
+	}
+	public init(apiError:OpenSplat_Error)
+	{
+		switch apiError
+		{
+			case OpenSplat_Error_Unknown:	message = "Unknown OpenSplat API error"
+			case OpenSplat_Error_Success:	message = "OpenSplat success(not an error)"
+			default:
+				message = "OpenSplat unknown-error [\(apiError)]"
+		}
+	}
+	
+	public var errorDescription: String?	{	message	}
+}
+
+public class OpenSplatTrainer : ObservableObject
+{
+	private var instance : Int32
+	var trainingTask : Task<Void,Error>!
+	@Published public var trainingError : Error? = nil
+	@Published public var trainingThreadFinished : Bool = false
+	public var isTraining : Bool	{	(trainingError == nil) && !trainingThreadFinished	}
+	
+	public init(projectPath:String)
+	{
+		instance = OpenSplat_AllocateInstanceFromPath(projectPath)
+		trainingTask = Task.detached(priority: .background)
+		{
+			do
+			{
+				try await self.Run()
+				DispatchQueue.main.async
+				{
+					self.trainingThreadFinished = true
+				}
+			}
+			catch
+			{
+				DispatchQueue.main.async
+				{
+					self.trainingError = error
+				}
+			}
+		}
+	}
+	
+	deinit
+	{
+		OpenSplat_FreeInstance(instance)
+	}
+	
+	func Run() async throws 
+	{
+		let error = OpenSplat_InstanceRunBlocking( instance )
+		if error != OpenSplat_Error_Success
+		{
+			throw OpenSplatError(apiError: error)
+		}
+	}
+	
+	public func GetSplats() throws -> [OpenSplat_Splat]
+	{
+		let splatCount = OpenSplat_GetSnapshot( instance, nil, 0 )
+		
+		var splatBuffer = [OpenSplat_Splat](repeating: OpenSplat_Splat(), count: Int(splatCount) )
+		let outputSplatCount = try splatBuffer.withUnsafeMutableBufferPointer
+		{
+			(buffer:inout UnsafeMutableBufferPointer<OpenSplat_Splat>) in
+			guard let bufferAddress = buffer.baseAddress else
+			{
+				throw OpenSplatError("Failed to get buffer address for \(splatCount) splats")
+			}
+			let outputSplatCount = OpenSplat_GetSnapshot( instance, bufferAddress, splatCount )
+			return outputSplatCount
+		}
+		
+		return splatBuffer
+	}
+	
+	public func RenderCamera(cameraIndex:Int,width:Int,height:Int) throws -> CGImage
+	{
+		let rgbBufferSize = width*height*3
+		var rgbBuffer = [UInt8](repeating: 99, count: rgbBufferSize )
+		try rgbBuffer.withUnsafeMutableBufferPointer
+		{
+			(buffer:inout UnsafeMutableBufferPointer<UInt8>) in
+			guard let bufferAddress = buffer.baseAddress else
+			{
+				throw OpenSplatError("Failed to get buffer address for rgb buffer")
+			}
+			let error = OpenSplat_RenderCamera( instance, Int32(cameraIndex), bufferAddress, Int32(rgbBufferSize), Int32(width), Int32(height) )
+			if error != OpenSplat_Error_Success
+			{
+				throw OpenSplatError(apiError: error)
+			}
+		}
+		
+		return try OpenSplatTrainer.rgbBufferToCGImage(&rgbBuffer,width:width,height: height)
+	}
+	
+	
+	public func GetCameraGroundTruthImage(cameraIndex:Int,width:Int,height:Int) throws -> CGImage
+	{
+		let rgbBufferSize = width*height*3
+		var rgbBuffer = [UInt8](repeating: 99, count: rgbBufferSize )
+		try rgbBuffer.withUnsafeMutableBufferPointer
+		{
+			(buffer:inout UnsafeMutableBufferPointer<UInt8>) in
+			guard let bufferAddress = buffer.baseAddress else
+			{
+				throw OpenSplatError("Failed to get buffer address for rgb buffer")
+			}
+			let error = OpenSplat_GetGroundTruthCameraImage( instance, Int32(cameraIndex), bufferAddress, Int32(rgbBufferSize), Int32(width), Int32(height) )
+			if error != OpenSplat_Error_Success
+			{
+				throw OpenSplatError(apiError: error)
+			}
+		}
+		//__export enum OpenSplat_Error	OpenSplat_RenderCamera(int TrainerInstance,int CameraIndex,uint8_t* ImageRgbBuffer,int ImageRgbBufferSize,int ImageRgbWidth,int ImageRgbHeight);
+		return try OpenSplatTrainer.rgbBufferToCGImage(&rgbBuffer,width:width,height: height)
+	}
+
+	static func rgbBufferToCGImage(_ rgbBuffer:inout[UInt8],width:Int,height:Int) throws -> CGImage
+	{
+		var convert = 
+		{
+			(rgb:inout vImage_Buffer,rgba:inout vImage_Buffer) in
+			let alphaBuffer : UnsafePointer<vImage_Buffer>? = nil
+			let alpha = Pixel_8(255)
+			let premultiply = false	//	true performs {r = (a * r + 127) / 255}
+			let flags = vImage_Flags(kvImageNoFlags)
+			let error = vImageConvert_RGB888toRGBA8888( &rgb, alphaBuffer, alpha, &rgba, premultiply, flags )
+			if error != kvImageNoError
+			{
+				throw OpenSplatError("Some RGB to RGBA error")
+			}
+		}
+			
+		
+		//	need to convert to RGBA for coregraphics
+		var rgbaBuffer = [UInt8](repeating: 77, count: width*height*4)
+		try rgbBuffer.withUnsafeMutableBytes
+		{
+			rgbBufferPointer in 
+			try rgbaBuffer.withUnsafeMutableBytes
+			{
+				rgbaBufferPointer in
+				var rgbImage = vImage_Buffer( data:rgbBufferPointer.baseAddress!, height:vImagePixelCount(height), width:vImagePixelCount(width), rowBytes: 3*width )
+				var rgbaImage = vImage_Buffer( data:rgbaBufferPointer.baseAddress!, height:vImagePixelCount(height), width:vImagePixelCount(width), rowBytes: 4*width )
+				
+				try convert( &rgbImage, &rgbaImage )
+			}
+		}
+				
+		
+		let bytesPerPixel = 4
+		let bytesPerRow = bytesPerPixel * width
+		let bitsPerComponent = 8
+		
+		
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
+		let alpha = CGImageAlphaInfo.premultipliedLast.rawValue
+		
+		let cgimage = try rgbBuffer.withUnsafeMutableBytes
+		{
+			rgbBufferPointer in
+			guard let context = CGContext(data: rgbBufferPointer.baseAddress!, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: alpha) else
+			{
+				throw OpenSplatError("Failed to create cg context")
+			}
+			
+			guard let cgImage = context.makeImage() else
+			{
+				throw OpenSplatError("Failed to create cg image")
+			}
+			return cgImage
+		}
+		
+		return cgimage
+	}
+	
+}
